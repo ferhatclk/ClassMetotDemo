cd51af142a7f4cb1742a44339617569c1e7a2aeb
FERHAT ÇELİK <ferhatcelik98@hotmail.com>
2021-02-28 20:36:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
